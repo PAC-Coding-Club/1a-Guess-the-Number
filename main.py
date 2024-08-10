274502ac339be294db51a36f66b003ef5ae6737d
MeltedButter77 <108335369+MeltedButter77@users.noreply.github.com>
2024-08-10 09:51:13 +0930

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+number = random.randint(0, 100)
+
+print("Im thinking of a number between 0-100, try guess it and ill let you know if your guess was too low or too high.")
+userInput = None
+while not userInput == number:
+    userInput = int(input())
+    if userInput > number:
+        print("Your guess was too HIGH")
+    elif userInput < number:
+        print("Your guess was too LOW")
+print("Well done! The number was %d" % number)
